e3cd9503820558f39ab3c92cece7626a07a1118f
rakovpublic <rakovpublic@gmail.com>
2023-02-22 22:09:37 +0200

    style to python

--- EtlSparkJob.py
+++ EtlSparkJob.py
@@ -3,16 +3,21 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName(
   "Etl parquet aggregation to json").getOrCreate()
 # with s3a:// prefix
-dfEntities = spark.read.parquet("s3a path")
-dfRelations = spark.read.parquet("s3a path")
-joinedDataFrame = dfEntities.join(dfRelations, dfEntities.entity_id == dfRelations.src, "left")
-joinedDataFrame = joinedDataFrame.rdd.map(lambda x: (x.entity_id, x)).groupByKey().mapValues(list).\
-  reduceByKey(lambda x, y: reduceByEntityIdToJson(x, y))
-joinedDataFrame.saveAsTextFile("s3a path")
+resolveable_record_types = []
+resolveable_record_types.append('PERSON')
+resolveable_record_types.append('COMPANY')
+resolveable_record_types.append('AIRCRAFT')
+resolveable_record_types.append('VESSEL')
+df_entities = spark.read.parquet("s3a path")
+df_relations = spark.read.parquet("s3a path")
+joined_data_frame = df_entities.join(df_relations, df_entities.entity_id == df_relations.src, "left")
+joined_data_frame = joined_data_frame.rdd.map(lambda x: (x.entity_id, x)).groupByKey().mapValues(list).\
+  reduceByKey(lambda x, y: reduce_by_entity_id_to_json(x, y))
+joined_data_frame.saveAsTextFile("s3a path")
 
 
 
 #will return string with json
-def reduceByEntityIdToJson(entityId,aggregations):
+def reduce_by_entity_id_to_json(entityId,aggregations):
   return
 
