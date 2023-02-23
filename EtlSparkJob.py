3c814a0c6ae9167ca869e4a0dbb22ca6d3ed2971
rakovpublic <rakovpublic@gmail.com>
2023-02-23 21:19:58 +0200

    I have started logic extraction

--- EtlSparkJob.py
+++ EtlSparkJob.py
@@ -1,3 +1,4 @@
+import csv
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName(
@@ -10,6 +11,9 @@ resolveable_record_types.append('AIRCRAFT')
 resolveable_record_types.append('VESSEL')
 df_entities = spark.read.parquet("s3a path")
 df_relations = spark.read.parquet("s3a path")
+analysis_mode = False
+id_sample_size = 1000000 if analysis_mode else 100
+codes_filename = ""
 joined_data_frame = df_entities.join(df_relations, df_entities.entity_id == df_relations.src, "left")
 joined_data_frame = joined_data_frame.rdd.map(lambda x: (x.entity_id, x)).groupByKey().mapValues(list).\
   reduceByKey(lambda x, y: reduce_by_entity_id_to_json(x, y))
